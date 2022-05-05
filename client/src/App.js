c07e268328beeb88f7c11a8c1841fb22176da1a5
Jianlog <alphaxie1988@gmail.com>
2022-05-05 14:34:54 +0800

    add anna photo

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,13 @@
 import logo from './logo.png';
 import './App.css';
+import BeforeAfterSlider from 'react-before-after-slider'
+
+
+const before = 'default.jpg'
+const jianlong = 'jianlong.jpg'
+const wesley = 'wesley.jpg'
+const jieyuan = 'jieyuan.jpg'
+
 
 function App() {
   return (
@@ -9,9 +17,37 @@ function App() {
       
       </header>
       <div className="Content">
-        <p>
-          Hello World Jie Yuan Wesley and Anna no filbert
-        </p>
+
+      <center>
+      <h1>Jianlong attempt</h1>
+      <BeforeAfterSlider
+        before={before}
+        after={jianlong}
+        width={960}
+        height={1280}
+      />
+      <h1>Jie Yuan attempt</h1>
+      <BeforeAfterSlider
+        before={before}
+        after={jieyuan}
+        width={960}
+        height={1280}
+      />
+      <h1>Wesley attempt</h1>
+      <BeforeAfterSlider
+        before={before}
+        after={wesley}
+        width={960}
+        height={1280}
+      />
+      <h1>Jieyuan vs Jianlong attempt</h1>
+      <BeforeAfterSlider
+        before={jieyuan}
+        after={wesley}
+        width={960}
+        height={1280}
+      />
+      </center>
       </div>
     </div>
   );
